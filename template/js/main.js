c1454b46cdfa271b404522d058ed892e5eb57d1a
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-10-15 11:29:39 +0100

    [#161204149] Create navigation menu, add favicon.png icon, add logo and sitename and create landing page structure

--- template/js/main.js
+++ template/js/main.js
@@ -2,7 +2,11 @@ const totalNum = 17;
 let imageNum = 1;
 
 const carousel = () => {
-  document.querySelector("#slider").setAttribute("src", `../images/ndubisi-kanu-park/test(${imageNum}).jpg`);
+  const slider = document.getElementById("slider");
+  slider.setAttribute("src", `../images/ndubisi-kanu-park/test(${imageNum}).png`);
+  // const sliderContainer = document.getElementById("slider-container");
+  // sliderContainer.style.backgroundImage = `url("../images/ndubisi-kanu-park/test(${imageNum}).png")`;
+  // sliderContainer.style.height = "1000px";
 
   imageNum++;
 
